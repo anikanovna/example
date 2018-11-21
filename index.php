25b355b4db71c8d37717d4c9d1e9bc154a78f8a7
Vasya <test@test.ru>
2018-11-21 15:21:43 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,138 @@
+<?php
+
+class User {
+	
+	protected $_name;
+	protected $_email;
+	protected $_type;
+	
+	public function __construct(string $name, string $email)
+	{
+		$this->_name  = $name;
+		$this->_email = $email;
+	}
+	
+	public function __toString()
+	{
+		return $this->_type . ' ' . $this->_name . ' ' . $this->_email;
+	}
+	
+	public function add()
+	{
+		$userStorage = fopen('users', 'a');
+		fwrite($userStorage, $this->_type . ' ' . $this->_name . ' ' . $this->_email . "\r\n");
+		fclose($userStorage);
+	}
+	
+	public function setName(string $name)
+	{
+		$this->_name  = $name;
+		
+		return $this;
+	}
+	
+	public function setEmail(string $email)
+	{
+		$this->_email = $email;
+		
+		return $this;
+	}
+	
+}
+
+
+class Customer extends User {
+	
+	protected $_type = 'Customer';	
+	
+	public function __toString()
+	{
+		return '<b>' . $this->_type . '</b>' . ' ' . $this->_name . ' ' . $this->_email;
+	}
+	
+}
+
+class Guest extends User {
+	
+	protected $_type = 'Guest';
+	
+	public function __toString()
+	{
+		return '<i style="color:red;">' . $this->_type . '</i>' . ' ' . $this->_name . ' ' . $this->_email;
+	}
+	
+}
+
+class Supplier extends User {
+	
+	protected $_type = 'Supplier';
+	
+	public function __toString()
+	{
+		return '<i style="color:green;">' . $this->_type . '</i>' . ' ' . $this->_name . ' ' . $this->_email;
+	}
+	
+}
+
+
+class Factory {
+	
+	public static function getUserObject(string $type, string $name, string $email)
+	{
+		return new $type($name, $email);
+	}
+	
+}
+
+class Users {
+		
+	private $_users;
+	
+	public function __construct()
+	{
+		$fileData = [];
+		
+		$userStorage = fopen('users', 'a+');
+		while ($row = fgets($userStorage)) {
+			$fileData[] = explode(' ', $row);
+		}
+		
+		foreach ($fileData as $data) {
+			$this->_users[] = Factory::getUserObject($data[0], $data[1], $data[2]);
+		}
+		
+		fclose($userStorage);
+	}
+	
+	public function show()
+	{
+		$view = '<table>';
+		foreach ($this->_users as $user) {
+			$view .= '<tr><td>' . $user . '</td></tr>';
+		}
+		$view .= '</table>';
+		
+		echo $view;
+	}
+	
+}
+
+
+$userObject = Factory::getUserObject('Customer', 'Vasya', 'boby@yandex.ru');
+$userObject->setName('Egor')
+	->add();
+
+$userObject = Factory::getUserObject('Guest', 'Petya', 'petya@yandex.ru');
+$userObject->add();
+
+$userObject = Factory::getUserObject('Customer', 'Vasya', 'boby@yandex.ru');
+$userObject->setName('Egor')
+	->add();
+
+$userObject = Factory::getUserObject('Guest', 'Petya', 'petya@yandex.ru');
+$userObject->add();
+
+$userObject = Factory::getUserObject('Supplier', 'Petya', 'petya@yandex.ru');
+$userObject->add();
+
+(new Users())->show();
